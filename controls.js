447189c63f8095ef616e28b73f46afc645929357
Sagnik Das <sagnikdas53@live.com>
2022-05-11 00:32:22 +0530

    will make the values update in css

---
+++ controls.js
@@ -0,0 +1,13 @@
+
+var blur = document.getElementById("blur").oninput
+    = function () {
+        console.log(this.value);
+    }
+document.getElementById("contrast").oninput =
+    function () {
+        console.log(this.value);
+    }
+document.getElementById("brightness").oninput
+    = function () {
+        console.log(this.value);
+    }
